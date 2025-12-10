a23b6817258b6728b9f52d1b1ceda42f23cab25b
Lynndroid21 <jwi.audio@hotmail.com>
2025-12-10 18:20:08 -0500

    edit waybar load

--- configs/sway/autorun-scripts/reload_waybar.sh
+++ configs/sway/autorun-scripts/reload_waybar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-killall waybar ; sleep 1 ; waybar -c ~/.config/sway/waybar/config.jsonc -s ~/.config/sway/waybar/lynn-style.css &
+killall waybar ; sleep 1 ; waybar -c ~/.config/sway/waybar/config.jsonc -s ~/.config/sway/waybar/default21-style.css &
 sleep 1
-waybar -c ~/.config/waybar/config-left.jsonc -s ~/.config/waybar/lynn-style.css & waybar -c ~/.config/waybar/config-right.jsonc -s ~/.config/waybar/lynn-style.css &
+waybar -c ~/.config/waybar/config-left.jsonc -s ~/.config/waybar/default21-style.css & waybar -c ~/.config/waybar/config-right.jsonc -s ~/.config/waybar/default21-style.css &
