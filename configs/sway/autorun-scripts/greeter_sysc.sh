1cc20d321220790a1fa8c6746ef1796059cb4349
Lynndroid21 <jwi.audio@hotmail.com>
2025-12-12 13:05:40 -0500

    Create greeter_sysc.sh

---
+++ configs/sway/autorun-scripts/greeter_sysc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+syscgo -effect beam-text -file ~/assets/Lynndroid21.txt -duration 20
+
+command clear
+
+sleep 2
+
+syscgo -effect fire-text -file ~/assets/Arch21.txt -duration 20
+
+command clear
+
+fastfetch
+
+exit
