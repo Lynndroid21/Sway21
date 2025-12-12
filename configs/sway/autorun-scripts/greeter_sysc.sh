46fb2a518a558b9917a24c209b0be5ad2c6e33f3
Lynndroid21 <jwi.audio@hotmail.com>
2025-12-12 13:20:39 -0500

    Update greeter_sysc.sh

--- configs/sway/autorun-scripts/greeter_sysc.sh
+++ configs/sway/autorun-scripts/greeter_sysc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-syscgo -effect beam-text -file ~/assets/Lynndroid21.txt -duration 20
+syscgo -effect beam-text -file ~/.config/sway/assets/Lynndroid21.txt -duration 20
 
 command clear
 
 sleep 2
 
-syscgo -effect fire-text -file ~/assets/Arch21.txt -duration 20
+syscgo -effect fire-text -file ~/.config/sway/assets/Arch21.txt -duration 20
 
 command clear
 
